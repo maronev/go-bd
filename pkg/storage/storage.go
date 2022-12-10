841039e4739ab01dc8d4986344929b2ca615f690
miguel.arone@outlook.com <miguel.arone@outlook.com>
2022-12-09 21:23:50 -0500

    commit inicial

---
+++ pkg/storage/storage.go
@@ -0,0 +1,33 @@
+package Storage
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"sync"
+)
+
+var (
+	db   *sql.DB
+	once sync.Once
+)
+
+func NewPostgresDB() {
+	once.Do(func() {
+		var err error
+		db, err := sql.Open("postgres", "postgres://admin:admin@localhost:5432/db-go?sslmode=disable")
+		if err != nil {
+			panic(err)
+		}
+
+		if err := db.Ping(); err != nil {
+			log.Fatalf("can't do ping: %v", err)
+		}
+
+		fmt.Println("Conectado a Postgres")
+	})
+}
+
+func Pool() *sql.DB {
+	return db
+}
